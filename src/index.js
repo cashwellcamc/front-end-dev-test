bdabe635eb42e787307d00b2834d04d2bd67fe86
Cameron Cashwell <Cameron.Cashwell@virginiapremier.com>
2019-09-20 14:44:57 -0400

    Start JS approach for hours operation

--- src/index.js
+++ src/index.js
@@ -11,11 +11,45 @@ document.addEventListener('DOMContentLoaded', () => {
     return response.json();
   })
   .then(function(myJson) {
-    console.log(JSON.stringify(myJson));
+    // console.log(JSON.stringify(myJson));
     const paragraph = document.getElementById('paragraph');
+
     if (typeof(paragraph) != 'undefined' && paragraph != null)
     {
       paragraph.innerHTML = myJson.acf['1yr_short_copy'];
     }
+    const hoursoperation = document.getElementById('hours-operation'); 
+    // if (typeof(hoursoperation) != 'undefined' && hoursoperation != null)
+    // {
+      console.log(myJson.acf['office_hours']);
+      console.log(myJson.acf['office_hours'][1]);
+
+      var d = new Date();
+      var dayOfWeek = d.getDay();
+      var hour = d.getHours();
+      console.log(d);
+      console.log(dayOfWeek);
+      console.log(hour);
+
+      // const now = new Date().toLocaleTimeString('en-CA', {
+      //   timeZone: 'America/Vancouver', hour12: false, timeStyle: 'short'
+      //   }).replace(':','');
+      // console.log(now)
+      //}
+
+      // Show  element
+      var show = function (hoursoperation) {
+        hoursoperation.style.display = 'block';
+      };
+      // Hide an element
+      var hide = function (hoursoperation) {
+        hoursoperation.style.display = 'none';
+      };
+      
+      // const isActive = starting_time <= closing_time   // test the shift type (normal or inverted)
+      // ? (starting_time <= now && closing_time > now)   // normal comparison
+      // : (starting_time <= now || closing_time > now);  // inverted comparison
+      // console.log(isActive)
+
   });
 });
